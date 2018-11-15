fa80b8d1eabeeff6100c5eaba823c80e0efe0883
Winfried Baumann <codewing@web.de>
2018-11-15 16:58:54 +0100

    initializing everything in sdl

--- src/engine/core/EngineCore.cpp
+++ src/engine/core/EngineCore.cpp
@@ -27,11 +27,11 @@ EngineCore::~EngineCore() {
 }
 
 void EngineCore::initSDLWindow(std::string windowName, glm::ivec2 windowSize) {
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_EVERYTHING);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    window = SDL_CreateWindow(windowName.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowSize.x, windowSize.y, SDL_WINDOW_OPENGL);
+    window = SDL_CreateWindow(windowName.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowSize.x, windowSize.y, SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
 }
 
 void EngineCore::runGame() {
