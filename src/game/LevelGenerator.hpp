f58b08bb6642fe6a64c64abddeea9741efcd1da3
wiba <wiba@itu.dk>
2018-12-14 13:16:42 +0100

    restructured header

--- src/game/LevelGenerator.hpp
+++ src/game/LevelGenerator.hpp
@@ -12,20 +12,21 @@ class WorldComponent;
 
 class LevelGenerator {
 public:
-    void generateLevel();
+	LevelGenerator(glm::vec2 levelSize, float earthPercentage);
+
+	void generateLevel();
 	void addTerrain(WorldComponent* world_comp);
 	void addIslands(WorldComponent* world_comp, int number);
 	void addIsland(WorldComponent* world_comp, int size, b2Vec2 position);
 	void addPortals(int couples);
-	std::vector<b2Vec2>  createIslandCenterPoints(int number);
+
+	std::vector<b2Vec2> createIslandCenterPoints(int number);
 	std::vector<int> createRandomIslandSizes(int number);
 	std::vector<b2Vec2> createTerrain(WorldComponent* world_comp);
 	std::vector<b2Vec2> createIslandPoints(int size, b2Vec2 islandCenter);
 
 	static constexpr float emptyTerrainRatio = 0.125f;
 	static constexpr float roundTerrainRatio = 0.375f;
-
-    LevelGenerator(glm::vec2 levelSize, float earthPercentage);
 
 private:
 	glm::vec2 levelSize;
